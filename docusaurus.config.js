d8d4737b900614bc2c76a5e2a791654b810e3d4c
riengchau <rieng.chau@galaxyfinx.com>
2026-05-17 20:14:27 +0700

    Initial commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -163,7 +163,7 @@ const config = {
         copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
       },
       prism: {
-        theme: prismThemes.github,
+        theme: prismThemes.oneDark,// [github, oneDark]
         darkTheme: prismThemes.dracula,
         additionalLanguages: [
           'ruby',
