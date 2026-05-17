10dd1cd8e5bf032a8153a8b7cfa2561b2d4fe28c
riengchau <rieng.chau@galaxyfinx.com>
2026-05-17 19:16:55 +0700

    Initial commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -125,42 +125,42 @@ const config = {
             items: [
               {
                 label: 'Tutorial',
-                to: '/docs/intro'
-              }
-            ]
+                to: '/docs/intro',
+              },
+            ],
           },
           {
             title: 'Community',
             items: [
               {
                 label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus'
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
               },
               {
                 label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus'
+                href: 'https://discordapp.com/invite/docusaurus',
               },
               {
                 label: 'X',
-                href: 'https://x.com/docusaurus'
-              }
-            ]
+                href: 'https://x.com/docusaurus',
+              },
+            ],
           },
           {
             title: 'More',
             items: [
               {
                 label: 'Blog',
-                to: '/blog'
+                to: '/blog',
               },
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus'
-              }
-            ]
-          }
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} Docusaurus Tailwind Shadcn. Templates by <a href="https://github.com/namnguyenthanhwork" style="font-weight: bold;" target="_blank">Thành Nam Nguyễn</a>`
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: prismThemes.github,
